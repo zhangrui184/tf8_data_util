da5f19daf81c91e7786b411319e2b0be3e7e8304
zhangrui184 <zhangrui184@mails.ucas.ac.cn>
2020-06-10 13:11:53 +0800

    first .bin to .txt

---
+++ replace_s_to_none.py
@@ -0,0 +1,29 @@
+#可以replace <s>和</s>，并且去掉空格，连成句子
+
+import sys
+
+
+#in_file="D:\python project me\data\my_piont_play/train/train_bin_to_txt3.txt"
+#out_file="D:\python project me\data\my_piont_play/train/train_bin_to_txt3_none.txt"
+
+def replace(in_file,out_file):
+    sents = []
+    with open(in_file, "r") as f1:
+        for line in f1.readlines():
+            line1 = line.replace("<s>", "")
+            line2 = line1.replace("</s>", "")
+            sents.append(line2)
+    with open(out_file, "w") as f2:
+        for sent in sents:
+            f2.write(sent)
+    sys.stderr.write('Done replacing\n')
+def jion(in_file,out_file):
+    sents=[]
+    with open(in_file, "r") as f1:
+        for line in f1.readlines():
+            line1 = line.replace(" ", "")
+            sents.append(line1)
+    with open(out_file,"w") as f2:
+        for sent in sents:
+            f2.write(sent)
+    sys.stderr.write('Done jioning\n')
